ff6b3da42d10f9133030f850d2b31acf72e731f8
Jarok Nguyen <57208974+jarok-nguyen@users.noreply.github.com>
2019-12-20 21:29:43 +0700

    comment unused function

--- web/src/page/Youtube.jsx
+++ web/src/page/Youtube.jsx
@@ -44,7 +44,7 @@ class Youtube extends React.Component {
 
   _onReady(event) {
     // access to player in all event handlers via event.target
-    event.target.playVideo();
+    //event.target.playVideo();
   }
 
   render() {
